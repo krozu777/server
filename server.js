0a8f9541a9ba13c608dfaf53b5991536e9f50db7
Yixzel Rodriguez Garcia <garmuz.yixzel@gmail.com>
2025-06-27 20:15:59 -0300

    primera actualizacion. server

---
+++ server.js
@@ -0,0 +1,13 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  console.log('Cliente conectado');
+
+  // cada 10 segundos lanza Sketch 3
+  setInterval(() => {
+    const trigger = JSON.stringify({ type: "launchSketch3" });
+    ws.send(trigger);
+    console.log("Trigger enviado");
+  }, 10000);
+});
